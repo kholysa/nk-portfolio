d6fdb9931ecc7cd189380063a4cce466b94adf1a
saifelkholy <saif.elkholy@sonder.com>
2025-07-16 23:21:23 -0400

    tssss

--- src/hooks/useImage.tsx
+++ src/hooks/useImage.tsx
@@ -1,16 +1,16 @@
 import {useEffect, useState} from 'react'
 
-const useImage = (fileName) => {
+const useImage = (fileName: string) => {
     const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(null)
-    const [image, setImage] = useState(null)
+    const [error, setError] = useState<any>(null)
+    const [image, setImage] = useState("")
 
     useEffect(() => {
         const fetchImage = async () => {
             try {
                 const response = await import(`../assets/img/${fileName}`) // change relative path to suit your needs
                 setImage(response.default)
-            } catch (err) {
+            } catch (err: any) {
                 setError(err)
             } finally {
                 setLoading(false)
