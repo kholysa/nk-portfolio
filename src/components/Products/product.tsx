eefcbbf32de5b2bf151673a2cc50c47abd96f601
saife <kholysa@gmail.com>
2026-04-16 16:11:01 -0400

    wip

--- src/components/Products/product.tsx
+++ src/components/Products/product.tsx
@@ -1,36 +1,51 @@
-import {useState} from "react";
+import { useEffect, useState } from "react";
 import {
-    StyledImage,
-    StyledProduct,
-    StyledProductDescription,
-    StyledProductTitle,
-    ModalContainer, ModalContent
+  StyledImage,
+  StyledProduct,
+  StyledProductDescription,
+  StyledProductTitle,
+  ModalContainer,
+  ModalContent,
 } from "./Products.css.tsx";
 import useImage from "../../hooks/useImage.tsx";
 
 export const Product = ({
-                            image,
-                            title,
-                            description,
-                        }: {
-    image: string;
-    title: string;
-    description: string;
+  image,
+  title,
+  description,
+}: {
+  image: string;
+  title: string;
+  description: string;
 }) => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const {image: imageSource} = useImage(image)
-    return (<>
-            <ModalContainer $isOpen={isModalOpen} onClick={() => setIsModalOpen(false)}>
-                <ModalContent onClick={() => setIsModalOpen(true)}>
-                    <StyledImage src={imageSource}/>
-                    <StyledProductTitle>{title}</StyledProductTitle>
-                    <StyledProductDescription>{description}</StyledProductDescription>
-                </ModalContent>
-            </ModalContainer>
-            <StyledProduct>
-                <StyledImage src={imageSource} onClick={() => setIsModalOpen(true)}/>
-                <StyledProductTitle>{title}</StyledProductTitle>
-            </StyledProduct>
-        </>
-    );
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { image: imageSource } = useImage(image);
+
+  useEffect(() => {
+    if (!isModalOpen) return;
+    const previous = document.body.style.overflow;
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = previous;
+    };
+  }, [isModalOpen]);
+
+  return (
+    <>
+      <ModalContainer
+        $isOpen={isModalOpen}
+        onClick={() => setIsModalOpen(false)}
+      >
+        <ModalContent onClick={() => setIsModalOpen(true)}>
+          <StyledImage src={imageSource} />
+          <StyledProductTitle>{title}</StyledProductTitle>
+          <StyledProductDescription>{description}</StyledProductDescription>
+        </ModalContent>
+      </ModalContainer>
+      <StyledProduct>
+        <StyledImage src={imageSource} onClick={() => setIsModalOpen(true)} />
+        <StyledProductTitle>{title}</StyledProductTitle>
+      </StyledProduct>
+    </>
+  );
 };
